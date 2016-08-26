3c86e4201c3a39e82951be5d95177ac1867f393f
phedoraj <phedoraj@users.noreply.github.com>
2016-08-25 23:58:08 -0400

    Edited functions midterm activities

--- functions/functions.js
+++ functions/functions.js
@@ -1,15 +1,48 @@
-// Invoke each function you write with an example and console.log the result
+
+Invoke each function you write with an example and console.log the result
 
 // multiply
-
+var multiply = function (a,b){
+  return a * b;
+};
+console.log(multiply (2*3)); 
 // whatForLunch
-
+var whatForLunch = function (food){
+  return 'name' + had + 'food' + 'for lunch today';
+};
+console.log("pizza");
 // isLargerThan100
+var isLargerThan100 = function (b){
+  return b;
+};
+console.log(200);
 
-// anyEvens
+};
+var nums = [1,2,4,5,7];
+var evens = [];
 
+var anyEvens = function(nums) {
+    for (var i = 0; i < nums.length; i++) {
+
+        if ((nums[i] % 2) != 1) {
+            evens.push(nums[i]);
+                console.log(evens);
+        }
+        else {
+            odds.push(nums[i]);
+                console.log(odds);
 // printDataType
-
+var printDataType = function (collection){
+    return (numbers);
+};
+console.log(collection[i]);
 // isDivisibleBy
-
+  var isDivisibleBy = function(num1){
+    return (an odd number);
+  };
+console.log(5);
 // fizzBuzz
+var fizzBuzz = function(){
+  return (string)
+};
+console.log("FizzBuzz");
