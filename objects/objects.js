b9a2b8d01bae33a4fd8c2d9e8a45dc033221d7d8
phedoraj <phedoraj@users.noreply.github.com>
2016-08-25 22:40:43 -0400

    Completed objects

--- objects/objects.js
+++ objects/objects.js
@@ -1,21 +1,31 @@
 // do not change lines 2 - 6:
 var twitter = {
+ 
   countCharacters: function(string) {
     return string.length;
   }
 };
 
 // 1. Dot notation:
-
+ stockprice.value = 18;
 // 2. Bracket notation:
-
+ceo['name']= 'Jack Dorsey';
 // 3. Practice on your own:
 var twittersAddress = "1355 Market St #900, San Francisco, CA 94103";
 
 // 4. Array:
-
+var twitterAddress = ["block number", 'street name', 'Suite number', 'city name', 'state', 'zip code'];
 // 5. Loop through array:
-
+for (var i = 0; i < array.length; i++);
 // 5. Invoke countCharacters:
-
+countCharacters(string);
 // 6. Loop through twitter object:
+var twitterAddress = {
+  for (key in )
+    block number: 1355,
+    street name: "Market Street" 
+    suite number: 900,
+    city name: "San Francisco"
+    state: "California"
+    zip code: 94103
+}
