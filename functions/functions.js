897b4879d62c06f666baa8494738d0298827376a
phedoraj <phedoraj@users.noreply.github.com>
2016-08-25 23:32:28 -0400

    Completed functions activity portion of midterm

--- functions/functions.js
+++ functions/functions.js
@@ -1,15 +1,37 @@
 // Invoke each function you write with an example and console.log the result
 
 // multiply
-
+var multiply = function (a,b){
+  return a * b;
+};
+console.log(multiply (2*3)); 
 // whatForLunch
-
+var whatForLunch = function (a){
+  return (a);
+};
+console.log("pizza");
 // isLargerThan100
-
+var isLargerThan100 = function (b){
+  return (b);
+};
+console.log(200);
 // anyEvens
-
+var anyEvens = function (e){
+    return (e);
+};
+console.log(4);
 // printDataType
-
+var printDataType = function (b){
+    return (boolean);
+};
+console.log(true);
 // isDivisibleBy
-
+  var isDivisibleBy = function(num1){
+    return (an odd number);
+  };
+console.log(5);
 // fizzBuzz
+var fizzBuzz = function(){
+  return (string)
+};
+console.log("FizzBuzz");
